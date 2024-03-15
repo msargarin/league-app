2dda8e69746447947f4bd5f7f0064d4a392730af
Mike <msargarin@users.noreply.github.com>
2024-03-15 20:37:09 +1100

    Create models and associated tests

---
+++ backend/league/models.py
@@ -0,0 +1,112 @@
+from decimal import Decimal
+
+from django.db import models
+from django.db.models import Q
+
+
+class Person(models.Model):
+    '''
+    A person has a name
+    '''
+    name = models.CharField(max_length=160)
+
+    class Meta:
+        abstract = True
+
+
+class Team(models.Model):
+    '''
+    A team has a name and a coach
+    '''
+    name = models.CharField(max_length=160)
+
+    def get_games(self):
+        return Game.objects.filter(Q(team_a=self) | Q(team_b=self))
+
+
+class Coach(Person):
+    '''
+    A coach is a person with a team
+    '''
+    team = models.OneToOneField(
+        Team,
+        on_delete=models.CASCADE,
+        related_name='coach')
+
+
+class Player(Person):
+    '''
+    A player is a person with a team
+    '''
+    team = models.OneToOneField(
+        Team,
+        on_delete=models.CASCADE,
+        related_name='players')
+    average_score = models.DecimalField(max_digits=4, decimal_places=1, default=Decimal('0.0'))
+    total_games_played = models.PositiveIntegerField(default=0)
+
+
+class Game(models.Model):
+    '''
+    A game has two opposing teams and their scores
+    '''
+    team_a = models.ForeignKey(
+        Team,
+        on_delete=models.PROTECT,
+        related_name='+')
+    team_a_score = models.PositiveIntegerField(
+        blank=True,
+        null=True)
+
+    team_b = models.ForeignKey(
+        Team,
+        on_delete=models.PROTECT,
+        related_name='+')
+    team_b_score = models.PositiveIntegerField(
+        blank=True,
+        null=True)
+
+    next_game = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        verbose_name='Next game in league',
+        related_name='previous_games',
+        null=True)
+
+    def get_winning_team(self):
+        winner = None
+        # Only check for a winner if there are scores
+        if self.team_a_score is not None and self.team_b_score is not None:
+            # Determine winner based on their scores
+            if self.team_a_score > self.team_b_score:
+                winner = self.team_a
+            else:
+                winner = self.team_b   # There are no ties in a basketball game!
+
+        return winner
+
+    def save(self, *args, **kwargs):
+        # Raise value error if only 1 team is specified
+        if self.team_a == self.team_b:
+            raise ValueError('Scores cannot be the same.')
+
+        # Raise value error if both teams have same score
+        if self.team_a_score == self.team_b_score:
+            raise ValueError('Scores cannot be the same.')
+
+        super().save(*args, **kwargs)
+
+
+class PlayedGame(models.Model):
+    '''
+    A player's score in a game he played
+    '''
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.PROTECT,
+        related_name='participants')
+    player = models.ForeignKey(
+        Player,
+        on_delete=models.PROTECT,
+        related_name='games_played')
+    score = models.PositiveIntegerField()
---
+++ backend/league/tests.py
@@ -0,0 +1,59 @@
+from django.db.models import Q
+from django.test import TestCase
+
+from .models import Team, Game
+
+
+class ModelTests(TestCase):
+    def test_game_different_teams(self):
+        '''
+        A game must always involve different teams
+        '''
+        # Create a team
+        team_a = Team.objects.create(name='Team A')
+
+        # Creating a game with only 1 team should raise an error
+        with self.assertRaises(ValueError):
+            Game.objects.create(
+                team_a=team_a,
+                team_b=team_a)
+
+    def test_game_no_tie(self):
+        '''
+        A game must always have a winner (ie. there are no tied games in basketball)
+        '''
+        # Create teams
+        team_a = Team.objects.create(name='Team A')
+        team_b = Team.objects.create(name='Team B')
+
+        # Creating a game with teams A and B ending in a tie should raise an error
+        with self.assertRaises(ValueError):
+            same_score = 100
+            Game.objects.create(
+                team_a=team_a,
+                team_a_score=same_score,
+                team_b=team_b,
+                team_b_score=same_score)
+
+    def test_team_list_games(self):
+        '''
+        A team's games must be enumerable
+        '''
+        # Create teams
+        team_a = Team.objects.create(name='Team A')
+        team_b = Team.objects.create(name='Team B')
+        team_c = Team.objects.create(name='Team C')
+
+        # Create games
+        Game.objects.bulk_create([
+            Game(team_a=team_a, team_a_score=100, team_b=team_b, team_b_score=110),  # Game with teams A and B
+            Game(team_a=team_c, team_a_score=100, team_b=team_a, team_b_score=110),  # Game with teams C and A
+            Game(team_a=team_b, team_a_score=100, team_b=team_c, team_b_score=110),  # Game with teams B and C
+        ])
+
+        # Team has a games parameter that returns all of their games
+        self.assertTrue(hasattr(team_a, 'get_games'))
+        self.assertQuerySetEqual(
+            Game.objects.filter(Q(team_a=team_a) | Q(team_b=team_a)),
+            team_a.get_games(),
+            ordered=False)
